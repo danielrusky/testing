3d40fd354c0f864863c3132a759d1ee2630d5f20
danila <spook9400@mail.ru>
2024-02-04 09:58:38 +0300

    add homework 19.2

---
+++ catalog/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+
+
+def home(request):
+    return render(request, 'catalog/home.html')
+
+
+def contact(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+        print(f'{name} ({phone}) : {message}')
+    return render(request, 'catalog/contacts.html')
+
+
+def index(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+        print(f'{name} {email} {message}')
+    return render(request, 'catalog/index.html')
